6358c5c1b4ba4dd71e48d3a1501214c8fddb584b
andknow <495609155@qq.com>
2022-03-15 23:24:53 +0800

    大概

--- Lab1/src/Sudoku/main.cc
+++ Lab1/src/Sudoku/main.cc
@@ -11,56 +11,6 @@
 #include "tools.h"
 #include "sudoku.h"
 
-/*
-int64_t now()
-{
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  return tv.tv_sec * 1000000 + tv.tv_usec;
-}
-
-int main(int argc, char* argv[])
-{
-  init_neighbors();
-
-  FILE* fp = fopen(argv[1], "r");
-  char puzzle[128];
-  int total_solved = 0;
-  int total = 0;
-  bool (*solve)(int) = solve_sudoku_basic;
-  if (argv[2] != NULL)
-    if (argv[2][0] == 'a')
-      solve = solve_sudoku_min_arity;
-    else if (argv[2][0] == 'c')
-      solve = solve_sudoku_min_arity_cache;
-    else if (argv[2][0] == 'd')
-      solve = solve_sudoku_dancing_links;
-  int64_t start = now();
-  while (fgets(puzzle, sizeof puzzle, fp) != NULL) {
-    if (strlen(puzzle) >= N) {
-      ++total;
-      input(puzzle);
-      init_cache();
-      //if (solve_sudoku_min_arity_cache(0)) {
-      //if (solve_sudoku_min_arity(0))
-      //if (solve_sudoku_basic(0)) {
-      if (solve(0)) {
-        ++total_solved;
-        if (!solved())
-          assert(0);
-      }
-      else {
-        printf("No: %s", puzzle);
-      }
-    }
-  }
-  int64_t end = now();
-  double sec = (end-start)/1000000.0;
-  printf("%f sec %f ms each %d\n", sec, 1000*sec/total, total_solved);
-
-  return 0;
-}
-*/
 //1. 定义所需变量，初始化线程
 //2. 设计生产者，消费者
 //3. 设计解题函数
@@ -162,7 +112,6 @@ void *put(void *argv)
     }
   
     printf(file_list.front());
-
 
     pthread_mutex_lock(&lock_file);
     strcpy(filename,file_list.front());
